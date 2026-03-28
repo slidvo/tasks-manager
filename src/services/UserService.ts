a9e4b62442d7807d625a46738d749f48429659e9
Viacheslav Kopeika <ks031088@gmail.com>
2026-03-28 20:27:54 +0500

    feat: add user in database with prisma orm

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,7 +1,11 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+
 import HttpStatusCodes from '@src/common/constants/HttpStatusCodes';
 import { RouteError } from '@src/common/utils/route-errors';
 import { IUser } from '@src/models/User.model';
 import UserRepo from '@src/repos/UserRepo';
+
+import { Post, PrismaClient } from '../../generated/prisma/client';
 
 /******************************************************************************
                                 Constants
@@ -11,6 +15,8 @@ const Errors = {
   USER_NOT_FOUND: 'User not found',
 } as const;
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL_API});
+const prisma = new PrismaClient({ adapter });
 /******************************************************************************
                                 Functions
 ******************************************************************************/
@@ -25,7 +31,20 @@ function getAll(): Promise<IUser[]> {
 /**
  * Add one user.
  */
-function addOne(user: IUser): Promise<void> {
+async function addOne(user: IUser): Promise<void> {
+  //TODO remove  UserRepo.add(user);
+  await prisma.user.create({
+    data: {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+    },
+  });
   return UserRepo.add(user);
 }
 
